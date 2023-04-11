2131ac83c0611c71d39d6ba87ea058caaa653ec4
pilinux <mahir.hasan@m.pilinux.me>
2023-04-11 13:45:31 +0200

    fix warning: title element received an array with more than 1 element as children
    Using template literals to combine the pid variable with the static string " | Next App" to create a single string that can be used as the child of the title element. This ensures that the title element has only one child.

--- src/pages/posts/[pid].tsx
+++ src/pages/posts/[pid].tsx
@@ -15,7 +15,7 @@ function Post() {
   return (
     <>
       <Head>
-        <title>{pid} | Next App</title>
+        <title>{`{pid} | Next App`}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
